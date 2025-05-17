ecb52fb396d2fc064401fc87545bee149d27a6b0
Ranapriyanshi <112609820+Ranapriyanshi@users.noreply.github.com>
2025-05-17 23:47:02 +0530

    new sentiment analysis model added

--- utils/sentiment_test.py
+++ utils/sentiment_test.py
@@ -2,24 +2,38 @@ from fastapi import APIRouter
 from services import ModelManager
 from pydantic import BaseModel
 import torch
+from transformers import pipeline
 
-tokenizer, model = ModelManager().load_model()
 sentiment_router = APIRouter()
-
+model_manager = ModelManager()
 class SentimentRequest(BaseModel):
     text: str
+    model: str
 
 @sentiment_router.post("/sentiment")
-def get_sentiment(input: SentimentRequest):
-    """
-    Dummy sentiment analysis function.
-    In a real-world scenario, this would call a sentiment analysis model.
-    """
-    inputs = tokenizer(input.text, return_tensors="pt", truncation=True, padding=True, max_length=512)
-    
-    with torch.no_grad():
-        outputs = model(**inputs)
-        predictions = torch.argmax(outputs.logits, dim=1).item()
-    # indentifier = pipeline("text-classification", model="bhadresh-savani/distilbert-base-uncased-emotion", return_all_scores=True)
-    # result = indentifier(text)
-    return {"text": input.text, "sentiment": predictions}
+def get_sentiment(request: SentimentRequest):
+    print(f"🔍 Requested model: {request.model}", flush=True)
+    print(f"📝 Text: {request.text}", flush=True)
+
+    try:
+        tokenizer, model = model_manager.load_model(request.model)
+        classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+        result = classifier(request.text)
+        return {"text": request.text, "model": request.model, "sentiment": result}
+    except Exception as e:
+        print("❌ Error:", str(e), flush=True)
+        return {"error": str(e)}
+
+
+# classifier = pipeline("zero-shot-classification")
+
+# # NEWS CATEGORIZATION OF ZEROSHOT CLASSIFICATION
+# sentences = [
+#     "India Beat Germany in a game of hockey by 3 goals",
+#     "It started pouring heavy rain in multiple cities of haryana including Amabal, Rohtak, Sonipat.",
+#     "Tension on Pak-india border is continuosly growing and things are getting bad after pak voilated ceasefire.",
+# ]
+# labels = ["Sports", "GeoPolitical", "Weather", "Politics", "Economy"]
+# for sentence in sentences:
+#     result = classifier(sentence, candidate_labels=labels)
+#     print(result)
