21f065985047614afebf15323bf4af273dcfa4ef
Ranapriyanshi <112609820+Ranapriyanshi@users.noreply.github.com>
2025-05-17 23:46:07 +0530

    Model Manger modified

--- services/model_manager.py
+++ services/model_manager.py
@@ -1,18 +1,26 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import os
 
-MODEL_ID = "j-hartmann/emotion-english-distilroberta-base"
-MODELS_DIR = os.path.join(os.path.abspath(__file__), '../..',"models")
-
+MODEL_MAP = {
+    "distilbert": "distilbert-base-uncased-finetuned-sst-2-english",
+    "bert": "bert-base-uncased",
+    "finbert": "ProsusAI/finbert"
+}
+# MODEL_ID = "j-hartmann/emotion-english-distilroberta-base"
+# MODELS_DIR = os.path.join(os.path.abspath(__file__), '../..',"models")
+c
 class ModelManager:
     def __init__(self):
-        self.tokenizer = None
-        self.model = None
+        self.models = {}
 
-    def load_model(self):
-        if self.model is None or self.tokenizer is None:
-            print("Loading model and tokenizer...")
-            self.tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, cache_dir=MODELS_DIR)
-            self.model = AutoModelForSequenceClassification.from_pretrained(MODEL_ID, cache_dir=MODELS_DIR)
-            self.model.eval()  # Optional: switch to inference mode
-        return self.tokenizer, self.model
+    def load_model(self, model_key=str):
+        if model_key not in MODEL_MAP:
+            raise ValueError(f"Model {model_key} not found in MODEL_MAP.")
+
+        if model_key not in self.models:
+            print("Loading model :{model_key}...", flush=True)
+            tokenizer = AutoTokenizer.from_pretrained(MODEL_MAP[model_key])
+            model = AutoModelForSequenceClassification.from_pretrained(MODEL_MAP[model_key])
+            self.models[model_key] = (tokenizer, model)
+
+        return self.models[model_key]
